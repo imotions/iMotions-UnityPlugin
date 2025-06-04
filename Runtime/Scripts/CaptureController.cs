1f1e8ea0c0b57e7999e256b1685ceeaf5af88850
Pedro Gomes <pedro.gomes@wavefunctionvr.com>
2025-06-04 15:46:41 +0100

    fix custom resolution + version update

--- Runtime/Scripts/CaptureController.cs
+++ Runtime/Scripts/CaptureController.cs
@@ -15,10 +15,14 @@ namespace Coflow.iMotionsPlugin.Meta
             captureComp = GetComponent<CaptureFromCamera>();
 
             captureComp.SetCamera(Camera.main, false);
+            captureComp.UseContributingCameras = false;
+            captureComp.CameraRenderResolution = CaptureBase.Resolution.Custom;
         }
 
         public void SetupResolution(int width, int height)
         {
+            captureComp.UseContributingCameras = false;
+            captureComp.CameraRenderResolution = CaptureBase.Resolution.Custom;
             captureComp.CameraRenderCustomResolution = new Vector2(width, height);
         }
 
